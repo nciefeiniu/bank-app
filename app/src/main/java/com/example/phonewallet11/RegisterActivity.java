891ecb2066013aa730a758dd0d5b2a548489fdfc
feiniu <tao.liu@epmap.org>
2022-10-25 14:07:54 +0800

    chore: 格式化代码

--- app/src/main/java/com/example/phonewallet11/RegisterActivity.java
+++ app/src/main/java/com/example/phonewallet11/RegisterActivity.java
@@ -78,26 +78,22 @@ public class RegisterActivity extends Activity {
 
         //创建播放器对象并设置音频/视频数据源
 
-        player=MediaPlayer.create(this,R.raw.ysyzc);
+        player = MediaPlayer.create(this, R.raw.ysyzc);
 
 
         // 为MediaPlayer对象添加完成事件监听器
-        player.setOnCompletionListener(new MediaPlayer.OnCompletionListener()
-
-        {
+        player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
-            public void onCompletion (MediaPlayer mp){
+            public void onCompletion(MediaPlayer mp) {
                 //重新开始播放
                 player.start();
 
             }
         });
         //设置按钮的单击事件监听器
-        play.setOnClickListener(new View.OnClickListener()
-
-        {
+        play.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick (View v){
+            public void onClick(View v) {
                 //获取SurfaceView控件的管理器SurfaceHolder
                 // 设置SurfaceHolder类型
                 // 将SurfaceView控件与MediaPlayer类进行关联
@@ -133,11 +129,9 @@ public class RegisterActivity extends Activity {
             }
         });
 
-        pause.setOnClickListener(new View.OnClickListener()
-
-        {
+        pause.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick (View v){
+            public void onClick(View v) {
                 /*判断当前是否正在播放并且没有处于暂停状态，
                 若是则暂停播放、设置暂停标记isPause为true并将“暂停”按钮文本内容设置为“继续”；
                 否则继续播放、设置暂停标记isPause为false并将“暂停”按钮文本内容设置为“暂停”
@@ -157,11 +151,9 @@ public class RegisterActivity extends Activity {
         });
 
 
-        stop.setOnClickListener(new View.OnClickListener()
-
-        {
+        stop.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick (View v){
+            public void onClick(View v) {
                 /* 判断播放器对象是否为null，若是则停止播放、
                 并在调用stop后设置再次通过start进行播放前调用prepare函数。
                  */
@@ -176,9 +168,10 @@ public class RegisterActivity extends Activity {
             }
         });
     }
+
     @Override
     protected void onDestroy() {
-        if(player.isPlaying()){
+        if (player.isPlaying()) {
             player.stop();   //停止音频的播放
         }
         player.release();     //释放资源
@@ -189,83 +182,82 @@ public class RegisterActivity extends Activity {
     //
 
 
-public void register(View v){
-        if(name.getText().toString().equals("")||(account.getText().toString()).equals("")||login_password.getText().toString().equals("")){
+    public void register(View v) {
+        if (name.getText().toString().equals("") || (account.getText().toString()).equals("") || login_password.getText().toString().equals("")) {
             Toast.makeText(RegisterActivity.this, "请先填写资料！", Toast.LENGTH_SHORT).show();
 
-        }else{
-            String ac=account.getText().toString();
-            DatebaseHelper dbHelper=new DatebaseHelper(getApplicationContext());
-            Cursor c=dbHelper.loginquery(ac);
+        } else {
+            String ac = account.getText().toString();
+            DatebaseHelper dbHelper = new DatebaseHelper(getApplicationContext());
+            Cursor c = dbHelper.loginquery(ac);
 
-            if(c.getCount()>0){
+            if (c.getCount() > 0) {
                 Toast.makeText(RegisterActivity.this, "账号已存在！", Toast.LENGTH_SHORT).show();
                 account.setText("");
-            }else{
-                int len=id.getText().toString().length();
-                if(account.getText().length()<6){
+            } else {
+                int len = id.getText().toString().length();
+                if (account.getText().length() < 6) {
                     Toast.makeText(RegisterActivity.this, "账号至少为6位字符", Toast.LENGTH_SHORT).show();
-                }else if(account.getText().length()>10){
+                } else if (account.getText().length() > 10) {
                     Toast.makeText(RegisterActivity.this, "账号长度过长", Toast.LENGTH_SHORT).show();
-                }else if(len<18){//正常 !=18  测试 >=18
+                } else if (len < 18) {//正常 !=18  测试 >=18
                     Toast.makeText(RegisterActivity.this, "请输入18位身份证号码", Toast.LENGTH_SHORT).show();
-                }else
-                    if(login_password.getText().toString().length()<6){
-                        Toast.makeText(RegisterActivity.this, "密码位数不足！", Toast.LENGTH_SHORT).show();
+                } else if (login_password.getText().toString().length() < 6) {
+                    Toast.makeText(RegisterActivity.this, "密码位数不足！", Toast.LENGTH_SHORT).show();
+                    password.setText("");
+                    login_password.setText("");
+                } else {
+                    if (!password.getText().toString().equals(login_password.getText().toString())) {
+                        Toast.makeText(RegisterActivity.this, "两次密码不一致！", Toast.LENGTH_SHORT).show();
                         password.setText("");
                         login_password.setText("");
-                    }else{
-                        if(!password.getText().toString().equals(login_password.getText().toString())){
-                            Toast.makeText(RegisterActivity.this, "两次密码不一致！", Toast.LENGTH_SHORT).show();
-                            password.setText("");
-                            login_password.setText("");
-                        }else{
+                    } else {
 
 
-                            RadioButton rb=(RadioButton) findViewById(rg.getCheckedRadioButtonId());
-                            String xbei=rb.getText().toString();
+                        RadioButton rb = (RadioButton) findViewById(rg.getCheckedRadioButtonId());
+                        String xbei = rb.getText().toString();
 
-                            final ContentValues values=new ContentValues();
-                            values.put("_id", id.getText().toString());
-                            values.put("account", account.getText().toString());
-                            values.put("name", name.getText().toString());
-                            values.put("sex",xbei);
-                            values.put("phone",phone.getText().toString());
-                            values.put("login_password", login_password.getText().toString());
-                            values.put("pay_password", pay_password.getText().toString());
-                            values.put("qx", "qwe");
-                            values.put("money", "0");
+                        final ContentValues values = new ContentValues();
+                        values.put("_id", id.getText().toString());
+                        values.put("account", account.getText().toString());
+                        values.put("name", name.getText().toString());
+                        values.put("sex", xbei);
+                        values.put("phone", phone.getText().toString());
+                        values.put("login_password", login_password.getText().toString());
+                        values.put("pay_password", pay_password.getText().toString());
+                        values.put("qx", "qwe");
+                        values.put("money", "0");
 
-                            final DatebaseHelper dbHelper1=new DatebaseHelper(getApplicationContext());
-                            String g=Integer.toString(dbHelper1.insert(values));
-                            Log.v("debug", g);
-                            //如果数据添加失败，返回值是-1
-                            if(!g.equals("-1")){
-                                final AlertDialog.Builder builder=new AlertDialog.Builder(this);
-                                builder.setMessage("注册成功，请登录！");
-                                builder.setPositiveButton("确认", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        //返回登录界面
-                                        Intent intent=getIntent();
-                                        setResult(1, intent);
-                                        finish();
-                                    }
-                                });
-                                builder.create().show();
+                        final DatebaseHelper dbHelper1 = new DatebaseHelper(getApplicationContext());
+                        String g = Integer.toString(dbHelper1.insert(values));
+                        Log.v("debug", g);
+                        //如果数据添加失败，返回值是-1
+                        if (!g.equals("-1")) {
+                            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                            builder.setMessage("注册成功，请登录！");
+                            builder.setPositiveButton("确认", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    //返回登录界面
+                                    Intent intent = getIntent();
+                                    setResult(1, intent);
+                                    finish();
+                                }
+                            });
+                            builder.create().show();
 
-                            }else{
-                                Toast.makeText(RegisterActivity.this, "您已申请过账号！", Toast.LENGTH_SHORT).show();
-                                account.setText("");
-                                id.setText("");
-                            }
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "您已申请过账号！", Toast.LENGTH_SHORT).show();
+                            account.setText("");
+                            id.setText("");
                         }
+                    }
 
                 }
 
-            }}
+            }
+        }
     }
-
 
 
 }
